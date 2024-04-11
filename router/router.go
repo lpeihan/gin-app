1fea003741d287ce2bbdaeeaaac225d5bd944f0d
Alfred Li <Alfred.Li@ringcentral.com>
2024-04-11 17:33:17 +0800

    feat: update logger

--- router/router.go
+++ router/router.go
@@ -3,11 +3,18 @@ package router
 import (
 	"gin-app/controllers"
 
+	"gin-app/utils"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+
+	//引用日志工具
+	r.Use(gin.LoggerWithConfig(utils.LoggerToFile()))
+	r.Use(utils.Recover)
+
 	api := r.Group("/api")
 
 	user := api.Group("/user")
